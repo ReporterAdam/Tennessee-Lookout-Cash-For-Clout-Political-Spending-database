be04827400c1bed50af95b1eea12fc82bc3c6c7c
Adam Friedman <80930524+ReporterAdam@users.noreply.github.com>
2026-04-27 08:51:18 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 (function () {
   'use strict';
 
-  const BASE_URL = 'https://cdn.jsdelivr.net/gh/ReporterAdam/Tennessee-Lookout-Cash-For-Clout-Political-Spending-database@fc2dc5204a336c3af3ae09709d3611dfffa1bb3f/';
+  const BASE_URL = 'https://cdn.jsdelivr.net/gh/ReporterAdam/Tennessee-Lookout-Cash-For-Clout-Political-Spending-database@2b6bbd02a85b63f419b0822421f25825929d04ec/';
   const DATA_URL = BASE_URL + 'data/';
   const JS_URL   = BASE_URL + 'js/';
 
